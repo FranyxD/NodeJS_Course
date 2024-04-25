0c03f101801391e49ab4f26011e4563f8ed0b70f
Francisco Fuertes Valls <82335974+FranyxD@users.noreply.github.com>
2024-04-25 15:07:20 +0000

    factory functions

--- 2 - Desarrollando en Node/src/js-foundation/05-factory.js
+++ 2 - Desarrollando en Node/src/js-foundation/05-factory.js
@@ -1,11 +1,16 @@
+"use strict";
+//usando {v4:uuidv4 estamos renombrando}
+const { v4: uuidv4 } = require('uuid');
+const getAge = require('get-age');
+
 const obj = { name: "John", birthdate: "2001-01-27" };
 
 const buildPerson = ({ name, birthdate }) => {
   return {
-    id: new Date().getTime(),
+    id: uuidv4(),
     name: name,
     birthdate: birthdate,
-    age: new Date().getFullYear() - new Date(birthdate).getFullYear(),
+    age: getAge(birthdate),
   };
 };
 
