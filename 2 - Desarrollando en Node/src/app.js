0a20f804d2e8b308d575a0bcedd3bcd14df7e564
Francisco Fuertes Valls <82335974+FranyxD@users.noreply.github.com>
2024-04-26 12:58:44 +0000

    factory functions Aplicado

--- 2 - Desarrollando en Node/src/app.js
+++ 2 - Desarrollando en Node/src/app.js
@@ -1,17 +1,30 @@
-const {emailTemplate} = require('./js-foundation/01-template');
-require('./js-foundation/02-destructuring');
-require('./js-foundation/03-callbacks');
-const {getUserById} = require('./js-foundation/03-callbacks');
-require('./js-foundation/05-factory');
+// const {emailTemplate} = require('./js-foundation/01-template');
+const {getAge, getId} = require('./plugins');
 
-//console.log(emailTemplate);
+// require('./js-foundation/02-destructuring');
+// require('./js-foundation/03-callbacks');
+// const {getUserById} = require('./js-foundation/03-callbacks');
+const { buildMakePerson } =  require('./js-foundation/05-factory');
 
-/* const id = 2;
-getUserById(id, (error, user) => {
-  if(error){
-    throw new Error(`USUARIO ${id} no encontrado`);
-  }
-  console.log({user});
-});
-*/
+const makePerson = buildMakePerson({getId, getAge})
+
+const obj = { name: "John", birthdate: "2001-01-27" };
+const john = makePerson( obj );
+
+console.log(john);
+
+
+
+
+// //console.log(emailTemplate);
+
+// /* const id = 2;
+// getUserById(id, (error, user) => {
+//   if(error){
+//     throw new Error(`USUARIO ${id} no encontrado`);
+//   }
+//   console.log({user});
+// });
+// */
+
 
--- 2 - Desarrollando en Node/src/js-foundation/05-factory.js
+++ 2 - Desarrollando en Node/src/js-foundation/05-factory.js
@@ -1,19 +1,24 @@
 "use strict";
 
-const {getAge, getId} = require('../plugins');
+//onst {getAge, getId} = require('../plugins');
 
 
-const obj = { name: "John", birthdate: "2001-01-27" };
+//const obj = { name: "John", birthdate: "2001-01-27" };
 
-const buildPerson = ({ name, birthdate }) => {
-  return {
-    id: getId(),
-    name: name,
-    birthdate: birthdate,
-    age: getAge(birthdate),
+const buildMakePerson = ({getId, getAge}) => {
+  return ({ name, birthdate }) => {
+
+    return {
+      id: getId(),
+      name: name,
+      birthdate: birthdate,
+      age: getAge(birthdate),
+    };
   };
-};
+}
 
-const john = buildPerson(obj);
 
-console.log(john)
+
+module.exports = {
+  buildMakePerson,
+}
