a63c3470778f80603cfd55f2b202b8188fcd54af
Francisco Fuertes Valls <82335974+FranyxD@users.noreply.github.com>
2024-06-21 10:26:50 +0000

    Arrange, Act y Assert

--- 2 - Desarrollando en Node (ahora con TS)/tests/app.test.ts
+++ 2 - Desarrollando en Node (ahora con TS)/tests/app.test.ts
@@ -1,6 +1,21 @@
-describe('App', () => {
-    //it('should be true', () => {
-    test('should be true', () => {
-        expect(true).toBe(true);
+//A A A
+
+describe('Test in the App file', () => {
+
+    //it('should be true', () => {   
+    test('should be 30', () => {
+        
+        //1. Arrange
+        const num1 = 10;
+        const num2 = 20;
+
+        //2. Act
+        const result = num1 + num2;
+
+        //3. Assert
+        expect(result).toBe(35);
+        
+
+
     })
 })
